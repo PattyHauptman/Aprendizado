d794d3309f11909f1e59e340bd03373f12cba3a2
Patrícia Hauptman <96195831+PattyHauptman@users.noreply.github.com>
2022-01-13 17:27:23 -0300

    4 - Quadrado
    Cálculo de área e perímetro

---
+++ quadrado.py
@@ -0,0 +1,9 @@
+entrada = input("Por favor, digite o valor correspondente ao lado de um quadrado: ")
+
+entradaconv = int(entrada)
+
+perimetro = entradaconv*4
+
+area = entradaconv**2
+
+print("perímetro:",perimetro,"- área:",area)
