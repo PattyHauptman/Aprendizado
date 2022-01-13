6e016a887814bc153af1ff3ea65b7c632456231b
Patrícia Hauptman <96195831+PattyHauptman@users.noreply.github.com>
2022-01-13 17:32:55 -0300

    6 - Informativo
    Texto informativo sobre data e valor de uma fatura.

---
+++ fatura.py
@@ -0,0 +1,7 @@
+NomeCliente = input("Por favor, digite o nome do cliente:")
+DiaVenc = input("Por favor, digite o dia do vencimento da fatura:")
+MesVenc = input("Por favor, digite o mês do vencimento da fatura:")
+Valor = input("Por favor, digite o valor da fatura:")
+
+print("Olá,",NomeCliente)
+print("A sua fatura com vencimento em",DiaVenc,"de", MesVenc,"no valor de R$",Valor,"está fechada.")
