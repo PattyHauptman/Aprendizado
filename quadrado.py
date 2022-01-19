b8f437fd540c91c741ad4b7a57ead7e0ab58268f
Patrícia Hauptman <96195831+PattyHauptman@users.noreply.github.com>
2022-01-19 17:11:31 -0300

    Update quadrado.py

--- quadrado.py
+++ quadrado.py
@@ -1,3 +1,9 @@
+# Receber um valor
+# Converter para números inteiros 
+# Criar variáveis já com o cálculo 
+# Retornar o valor
+
+
 entrada = input("Por favor, digite o valor correspondente ao lado de um quadrado: ")
 
 entradaconv = int(entrada)
